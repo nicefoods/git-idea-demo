99cfeb257c2afe282964f6c855325125308ab99f
wpc <1078672889@qq.com>
2022-05-04 23:35:01 +0800

    push test2

--- src/main/java/com/wpc/git/test/gitTest.java
+++ src/main/java/com/wpc/git/test/gitTest.java
@@ -6,5 +6,11 @@ public class gitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
+
     }
 }
